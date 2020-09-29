d8a74409b990a3aaebc8f4a2b1da2914e82d7253
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2020-09-29 09:05:39 +0200

    re-use logic to load and resize image

--- cmd/fingerprint-corpus/main.go
+++ cmd/fingerprint-corpus/main.go
@@ -1,72 +1,19 @@
 package main
 
 import (
-	"image"
-	"image/color"
 	_ "image/jpeg"
 	"image/png"
 	"log"
 	"os"
 	"path"
 
+	"github.com/alevinval/fingerprints/internal/cmdhelper"
 	"github.com/alevinval/fingerprints/internal/kernel"
 	"github.com/alevinval/fingerprints/internal/matrix"
 	"github.com/alevinval/fingerprints/internal/processing"
-	"github.com/nfnt/resize"
 )
 
 var outFolder = "out"
-
-func resizeImage(img image.Image) image.Image {
-	maxDimension := 300
-	dx := img.Bounds().Dx()
-	dy := img.Bounds().Dy()
-	if dx < maxDimension && dy < maxDimension {
-		return img
-	}
-
-	xp := 0
-	yp := 0
-	if dx > dy {
-		xp = maxDimension
-		yp = int(float64(dy) / (float64(dx) / float64(maxDimension)))
-	} else if dy > dx {
-		yp = maxDimension
-		xp = int(float64(dx) / (float64(dy) / float64(maxDimension)))
-	} else {
-		xp, yp = maxDimension, maxDimension
-	}
-	log.Printf("resizing image from (%d,%d) to (%d,%d)", dx, dy, xp, yp)
-	return resize.Resize(uint(xp), uint(yp), img, resize.Bilinear)
-}
-
-func loadImage(name string) *image.Gray {
-	f, err := os.Open(name)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	img, _, err := image.Decode(f)
-	if err != nil {
-		panic(err)
-	}
-
-	resizedImg := resizeImage(img)
-
-	bounds := resizedImg.Bounds()
-	w, h := bounds.Max.X, bounds.Max.Y
-	gray := image.NewGray(bounds)
-	for x := 0; x < w; x++ {
-		for y := 0; y < h; y++ {
-			oldColor := resizedImg.At(x, y)
-			grayColor := color.GrayModel.Convert(oldColor)
-			gray.Set(x, y, grayColor)
-		}
-	}
-
-	return gray
-}
 
 func showImage(title string, in *matrix.M) {
 	f, err := os.Create(path.Join(outFolder, title+".png"))
@@ -128,7 +75,7 @@ func processImage(in *matrix.M) {
 }
 
 func main() {
-	original := loadImage("corpus/nist3.jpg")
+	original := cmdhelper.LoadImage("corpus/nist3.jpg")
 	img := matrix.NewFromGray(original)
 	processImage(img)
 }
---
+++ internal/cmdhelper/load_image.go
@@ -0,0 +1,61 @@
+package cmdhelper
+
+import (
+	"image"
+	"image/color"
+	"log"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+func resizeImage(img image.Image) image.Image {
+	maxDimension := 300
+	dx := img.Bounds().Dx()
+	dy := img.Bounds().Dy()
+	if dx < maxDimension && dy < maxDimension {
+		return img
+	}
+
+	xp := 0
+	yp := 0
+	if dx > dy {
+		xp = maxDimension
+		yp = int(float64(dy) / (float64(dx) / float64(maxDimension)))
+	} else if dy > dx {
+		yp = maxDimension
+		xp = int(float64(dx) / (float64(dy) / float64(maxDimension)))
+	} else {
+		xp, yp = maxDimension, maxDimension
+	}
+	log.Printf("resizing image from (%d,%d) to (%d,%d)", dx, dy, xp, yp)
+	return resize.Resize(uint(xp), uint(yp), img, resize.Bilinear)
+}
+
+func LoadImage(name string) *image.Gray {
+	f, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	resizedImg := resizeImage(img)
+
+	bounds := resizedImg.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	gray := image.NewGray(bounds)
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			oldColor := resizedImg.At(x, y)
+			grayColor := color.GrayModel.Convert(oldColor)
+			gray.Set(x, y, grayColor)
+		}
+	}
+
+	return gray
+}
