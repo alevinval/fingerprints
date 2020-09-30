fa096e664c4d988122b9eb319abe91c99f417df0
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2020-09-30 11:43:13 +0200

    generalize frame extraction

--- internal/processing/frame.go
+++ internal/processing/frame.go
@@ -8,72 +8,106 @@ import (
 	"github.com/alevinval/fingerprints/internal/types"
 )
 
+type Axis byte
+
+const (
+	xAxis Axis = iota
+	yAxis
+)
+
 func ExtractFrame(binarizedSegmented *matrix.M) types.Frame {
-	v := findVerticalAxis(binarizedSegmented)
-	h := findHorizontalAxis(binarizedSegmented)
+	h := findHorizontalAxis(binarizedSegmented, false)
+	v := findVerticalAxis(binarizedSegmented, false)
 	return types.Frame{Horizontal: h, Vertical: v}
 }
 
-func findVerticalAxis(binarizedSegmented *matrix.M) image.Rectangle {
-	bounds := binarizedSegmented.Bounds()
-	longestX := 0
-	x1, y0, y1 := 0, 0, 0
-	for x := bounds.Min.X; x < bounds.Max.X; x++ {
-		c := 0
-		_y0 := 0
-		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-			v := binarizedSegmented.At(x, y)
-			if c == 0 && v < 125 {
-				continue
-			} else if v > 125 && _y0 == 0 {
-				_y0 = y
-				c++
-			} else if v > 125 {
-				c++
-			} else {
-				if c > longestX {
-					longestX = c
-					y0 = _y0
-					x1 = x
-					y1 = y
-				}
-			}
-		}
+func providePoints(bounds image.Rectangle, axis Axis, isReversed bool, f func(n int)) {
+	var ini, max int
+	if axis == xAxis {
+		ini = bounds.Min.X
+		max = bounds.Max.X
+	} else {
+		ini = bounds.Min.Y
+		max = bounds.Max.Y
 	}
 
-	frame := image.Rect(x1, y0, x1, y1)
+	if isReversed {
+		for n := max - 1; n >= ini; n-- {
+			f(n)
+		}
+	} else {
+		for n := ini; n < max; n++ {
+			f(n)
+		}
+	}
+}
+
+func findVerticalAxis(binarizedSegmented *matrix.M, isReversed bool) image.Rectangle {
+	frame := findAxis(binarizedSegmented, xAxis, yAxis, isReversed)
 	log.Printf("vertical frame: %s", frame)
+	return frame
+
+}
+
+func findHorizontalAxis(binarizedSegmented *matrix.M, isReversed bool) image.Rectangle {
+	frame := findAxis(binarizedSegmented, yAxis, xAxis, false)
+	log.Printf("horizontal frame: %s", frame)
 	return frame
 }
 
-func findHorizontalAxis(binarizedSegmented *matrix.M) image.Rectangle {
-	bounds := binarizedSegmented.Bounds()
+func findAxis(in *matrix.M, firstAxis, secondAxis Axis, isReversed bool) image.Rectangle {
+	bounds := in.Bounds()
 	longestY := 0
-	x0, x1, y1 := 0, 0, 0
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+	a0, b0, b1 := 0, 0, 0
+	providePoints(bounds, firstAxis, isReversed, func(a int) {
 		c := 0
-		_x0 := 0
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			v := binarizedSegmented.At(x, y)
+		_b0 := 0
+		providePoints(bounds, secondAxis, false, func(b int) {
+			var v float64
+			if firstAxis == xAxis {
+				v = in.At(a, b)
+			} else {
+				v = in.At(b, a)
+			}
 			if c == 0 && v < 125 {
-				continue
-			} else if v > 125 && _x0 == 0 {
-				_x0 = x
+				// do nothing
+			} else if v > 125 && _b0 == 0 {
+				_b0 = b
 				c++
 			} else if v > 125 {
 				c++
 			} else {
 				if c > longestY {
 					longestY = c
-					x0 = _x0
-					x1 = x
-					y1 = y
+					a0 = a
+					b0 = _b0
+					b1 = b
 				}
 			}
-		}
-	}
+		})
+	})
 
-	frame := image.Rect(x0, y1, x1, y1)
-	log.Printf("horizontal frame: %s", frame)
+	var frame image.Rectangle
+	if firstAxis == xAxis {
+		frame = image.Rect(a0, b0, a0, b1)
+	} else {
+		frame = image.Rect(b0, a0, b1, a0)
+	}
 	return frame
+}
+
+func mergeFrame(a, b types.Frame) types.Frame {
+	return types.Frame{
+		Horizontal: geometricMean(a.Horizontal, b.Horizontal),
+		Vertical:   geometricMean(a.Vertical, b.Vertical),
+	}
+}
+
+func geometricMean(a, b image.Rectangle) image.Rectangle {
+	return image.Rect(
+		int((a.Min.X+b.Min.X)/2),
+		int((a.Min.Y+b.Min.Y)/2),
+		int((a.Max.X+b.Max.X)/2),
+		int((a.Max.Y+b.Max.Y)/2),
+	)
 }
