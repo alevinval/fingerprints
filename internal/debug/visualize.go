69c331cc06b0e0bfc72858100ef5cbd77a74c33b
Alexandre Vinyals Valdepeñas <alevinval@gmail.com>
2020-09-30 10:36:39 +0200

    draw frame as a cross

--- internal/debug/visualize.go
+++ internal/debug/visualize.go
@@ -25,8 +25,8 @@ func DrawFeatures(original image.Image, result *types.DetectionResult) {
 }
 
 func drawFrame(dst draw.Image, r image.Rectangle, c color.Color) {
-	drawFillSquare(dst, r.Bounds().Min.X, r.Bounds().Min.Y, c)
-	drawFillSquare(dst, r.Bounds().Max.X, r.Bounds().Max.Y, c)
+	drawCross(dst, r.Bounds().Min.X, r.Bounds().Min.Y, c)
+	drawCross(dst, r.Bounds().Max.X, r.Bounds().Max.Y, c)
 }
 
 func drawFillSquare(dst draw.Image, x, y int, c color.Color) {
@@ -39,4 +39,12 @@ func drawFillSquare(dst draw.Image, x, y int, c color.Color) {
 	dst.Set(x-1, y, c)
 	dst.Set(x-1, y-1, c)
 	dst.Set(x-1, y+1, c)
+}
+
+func drawCross(dst draw.Image, x, y int, c color.Color) {
+	dst.Set(x, y, c)
+	dst.Set(x, y-1, c)
+	dst.Set(x, y+1, c)
+	dst.Set(x+1, y, c)
+	dst.Set(x-1, y, c)
 }
